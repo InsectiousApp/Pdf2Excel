b532425751bbdc9e604d5cec4e97c4d714cf140c
chetancodev95 <chetanmann95@gmail.com>
2016-06-23 02:08:16 +0530

    working on file 2 using pypdf2

--- file1.py
+++ file1.py
@@ -1,6 +1,5 @@
 from PyPDF2 import PdfFileReader
-
-f = open('CT1-All.pdf', 'rb')
+f = open('samplepdf1.pdf', 'rb')
 reader = PdfFileReader(f)
 contents = reader.getPage(0).extractText().split('\n')
 f.close()
---
+++ file2.py
@@ -0,0 +1,5 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
+output = PdfFileWriter()
+
+ipdf = PdfFileReader(open('samplepdf1.pdf', 'rb'))
+watermark = ipdf.getPage(0)
